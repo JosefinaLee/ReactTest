a6cfedf396d31bb69603585ff5559e2f80bcf701
eliselee <714448530@qq.com>
2016-08-23 22:58:45 +0800

    Set range of images
    Set img ranges by using componentDidMount

--- photoWall/component/Photowall.js
+++ photoWall/component/Photowall.js
@@ -21,21 +21,77 @@
 		}
 	});
 	var Photowall = React.createClass({
+		Ranges: {
+			center: {
+				left: 0,
+				top: 0
+			},
+			imgRange: {
+				leftRangeX: [0, 0],
+				rightRangeX: [0, 0],
+				rangeY: 0
+			}
+		},
+
+		setRanges: function(){
+			var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+				imgDOM = React.findDOMNode(this.refs.img0);
+			var stgW = stageDOM.clientWidth,
+				stgH = stageDOM.clientHeight,
+				imgW = imgDOM.clientWidth,
+				imgH = imgDOM.clientHeight,
+				hstgW = stgW / 2,
+				hstgH = stgH / 2,
+				himgW = imgW / 2,
+				himgH = imgH / 2;
+			this.Ranges.center = {
+				left: hstgW - himgW,
+				top: hstgH - himgH
+			};
+			this.Ranges.imgRange = {
+				leftRangeX: [-himgW, hstgW - 3*himgW],
+				rightRangeX: [hstgW + himgW, stgW - himgW],
+				y: [-himgH, stgH - himgH]
+			};
+			console.log(this.Ranges);
+		},
+
+		getInitialState: function(){
+			return {
+				imgInfos: {
+					top: 0,
+					left: 0
+				}
+			}
+		},
+
+		rearrange: function(){
+			var rearrangeInfo = this.state.imgInfos;
+		},
+
 		render: function(){
 			var imgArr = [],
 				navArr = [];
 			imgDatas.forEach(function(elem, index){
-				imgArr.push(<ImgApp data={elem} key={index}/>);
+				imgArr.push(<ImgApp data={elem} key={index} ref={"img"+index}/>);
 				navArr.push(<NavApp key={index}/>);
 			});
 
 			return (
-				<section>
-					{imgArr}
-					{navArr}
+				<section ref="stage">
+					<section>
+						{imgArr}
+					</section>
+					<nav>
+						{navArr}
+					</nav>
 				</section>
 				);
-		}		
+		},
+		componentDidMount: function(){
+			this.setRanges();
+		}
+
 	});
 
 	ReactDOM.render(
